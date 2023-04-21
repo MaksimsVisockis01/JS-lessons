a9d414be12d26a3cf42b6cfc35e721f65b4c1c54
Maksims Visockis <Vezun4ik2004@gmail.com>
2023-04-21 23:05:03 +0300

    2

--- main.js
+++ main.js
@@ -52,4 +52,45 @@
 
 //----------------------
 
-const words = ''
+// const words = 'Words, are, splited';
+// console.log(words.split(', ')); // splited every ", "
+//Array(3) 0: "Words" 1: "are" 2: "splited"
+
+//----------------------
+
+//Arrays - variables that hold multiple values
+
+// const numbers = new Array(1,2,3,4,5);
+// console.log(numbers);//(5) [1, 2, 3, 4, 5]
+
+
+//Better option
+// const fruits = ['apples', 'oranges', 'pears'];
+// console.log(fruits); //(3) ['apples', 'oranges', 'pears'];
+
+// const anytypes = ['apples', 'oranges', 'pears', 10, true, false, null];
+// console.log(anytypes); // we can contain any data in arrays
+
+// const anytypes = ['apples', 'oranges', 'pears', 10, true, false, null];
+// console.log(anytypes[1]); //oranges (array: 0, 1, 2, 3 ....)
+
+
+//we can change and add dates in array, but cant change array 
+
+// const array = ['apples', 'oranges', 'pears', 10, true, false, null];
+// array[3] = 'bubble';
+//  console.log(array[3]);// bubble
+
+// const array = ['apples', 'oranges', 'pears', 10, true, false, null];
+// array = [];
+// console.log(array);// error
+
+
+//we can use methods for arrays:
+
+const array = ['apples', 'oranges', 'pears', 10, true, false, null];
+
+array.push('end')//push values into the end
+array.unshift('start')//push values into the begining
+array.pop();//take away last value from array
+console.log(array);
